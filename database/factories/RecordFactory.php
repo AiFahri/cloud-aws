64904b32ee817f6be909c5e7646bccebda446bdb
AiFahri <fahrinurilll@gmail.com>
2025-11-12 11:35:19 +0700

    refactor:  define model and add default state attributes

--- database/factories/RecordFactory.php
+++ database/factories/RecordFactory.php
@@ -2,22 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Record;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Record>
- */
 class RecordFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
+    protected $model = Record::class;
+
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(3),
+            'content' => fake()->paragraph(2),
         ];
     }
 }
