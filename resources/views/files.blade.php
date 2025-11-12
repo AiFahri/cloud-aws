39733703f41df67bb9d0b2b0d1b001de248147ab
AiFahri <fahrinurilll@gmail.com>
2025-11-12 13:55:37 +0700

    feat: enhance file upload view with error handling and improved file listing

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -3,7 +3,16 @@
 <h4>Upload File</h4>
 <form action="/files" method="POST" enctype="multipart/form-data">
   @csrf
-  <input type="file" name="file" class="form-control mb-3" required>
+  <input type="file" name="file" class="form-control mb-3 @error('file') is-invalid @enderror" required accept="image/*,application/pdf,.doc,.docx,.txt,.zip,.rar">
+  @if($errors->any())
+    <div class="alert alert-danger mb-3">
+      <ul class="mb-0">
+        @foreach($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
   <button class="btn btn-success">Upload</button>
 </form>
 
@@ -12,9 +21,15 @@
 @endif
 
 <h5 class="mt-4">Uploaded Files</h5>
-<ul>
-@foreach($files as $f)
-  <li>{{ basename($f) }}</li>
-@endforeach
-</ul>
+@if(isset($error))
+  <div class="alert alert-danger">{{ $error }}</div>
+@elseif(count($files) > 0)
+  <ul>
+  @foreach($files as $f)
+    <li>{{ basename($f) }}</li>
+  @endforeach
+  </ul>
+@else
+  <p class="text-muted">Belum ada file yang diupload.</p>
+@endif
 @endsection
